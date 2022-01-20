1c4f365b5d98634bcd533e8295ca56803289b653
Alessandro Scandone <alescandone@gmail.com>
2022-01-20 23:22:49 +0100

    added login functionality

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,21 +2,24 @@
 import Button from '@/components/Button.vue'
 import Input from '@/components/Input.vue'
 import FieldSet from '@/components/FieldSet.vue'
-import { useValidation } from '@/useValidation'
+import Alert from '@/components/Alert.vue'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { useEmailValidation, usePasswordValidation } from '@/validations'
 
-const email = useValidation((field) => {
-  if (!field.includes('@')) {
-    return 'Insert a valid email'
-  }
-})
+const router = useRouter()
 
-const password = useValidation((field) => {
-  if (field === '') {
-    return 'Insert a valid password'
-  }
-})
+type AuthResponse =
+  | { type: 'NOT_ASKED' }
+  | { type: 'LOADING' }
+  | { type: 'GOT_ERROR'; message: string }
 
-function onSubmit() {
+const authResponse = ref<AuthResponse>({ type: 'NOT_ASKED' })
+const email = useEmailValidation()
+const password = usePasswordValidation()
+
+async function onSubmit() {
   email.validate()
   password.validate()
 
@@ -25,6 +28,24 @@ function onSubmit() {
     password.validationMsg === undefined
   ) {
     console.log(email.value, password.value)
+    authResponse.value = { type: 'LOADING' }
+    const auth = getAuth()
+    try {
+      const creds = await signInWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      )
+
+      console.log(creds)
+
+      router.push('/')
+    } catch (e) {
+      authResponse.value = {
+        type: 'GOT_ERROR',
+        message: (e as Error).message,
+      }
+    }
   }
 }
 </script>
@@ -33,6 +54,14 @@ function onSubmit() {
   <div class="px-5 py-2">
     <div class="max-w-sm mx-auto">
       <h3 class="font-bold text-2xl mt-2 mb-4">Login</h3>
+
+      <div v-if="authResponse.type === 'GOT_ERROR'">
+        <Alert type="error">
+          {{ authResponse.message }}
+        </Alert>
+        <div class="h-5"></div>
+      </div>
+
       <form @submit.prevent="onSubmit" class="space-y-4">
         <FieldSet id="email">
           <template #label>Email</template>
