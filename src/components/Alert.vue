e5b1adcb1565ae984cb8ad9887397d649a3f5085
Alessandro Scandone <alescandone@gmail.com>
2022-01-20 22:56:39 +0100

    added Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+export type Type = 'default' | 'error'
+
+export interface Props {
+  type?: Type
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'default',
+})
+</script>
+<template>
+  <div
+    :class="{
+      'border-l-4 px-4 py-3 rounded-r': true,
+      'border-blue-900 text-blue-900 bg-blue-100': props.type === 'default',
+      'border-red-900 text-red-900 bg-red-100': props.type === 'error',
+    }"
+  >
+    <slot />
+  </div>
+</template>
