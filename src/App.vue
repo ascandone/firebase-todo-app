f75db447d656c5e2fb64fdb5e7efa596b82a9d13
Alessandro Scandone <alescandone@gmail.com>
2022-01-22 17:47:12 +0100

    improved load time

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 import { RouterView } from 'vue-router'
 import { onAuthStateChanged, getAuth } from 'firebase/auth'
 import { useAuth } from '@/stores/auth'
+import { ref } from 'vue'
 
 const store = useAuth()
 
 const auth = getAuth()
 
+const loadingUser = ref(true)
 onAuthStateChanged(auth, (user) => {
   store.user = user
+  loadingUser.value = false
 })
 </script>
 
 <template>
-  <RouterView></RouterView>
+  <RouterView v-if="!loadingUser"></RouterView>
 </template>
