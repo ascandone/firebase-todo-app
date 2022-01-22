b9847f98af3640d758e5e267c7177b252f6a3743
Alessandro Scandone <alescandone@gmail.com>
2022-01-22 18:21:05 +0100

    edit input style

--- src/components/inputClass.ts
+++ src/components/inputClass.ts
@@ -1,7 +1,7 @@
 export const inputClass = (error: boolean) => ({
   'rounded-md w-full px-4 py-2 leading-none text-gray-900 outline-none resize-none':
     true,
-  'bg-gray-100': !error,
+  'bg-gray-200 bg-opacity-80': !error,
   'bg-red-100': error,
   'hover:bg-transparent hover:ring ring-pink-200': true,
   'transition-all duration-150': true,
