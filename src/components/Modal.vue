68b706741c5b85223a45c98d8ecef3d5e70f198b
Alessandro Scandone <alescandone@gmail.com>
2022-01-23 12:36:29 +0100

    added modal entering animation

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -17,20 +17,27 @@ const emit = defineEmits<Emits>()
     class="absolute bg-gray-800 bg-opacity-40 w-full inset-0 z-40 p-4"
     @click.self="emit('closed-modal')"
   >
-    <div class="mx-auto max-w-md bg-white rounded-md px-4 py-3">
-      <div class="flex">
-        <h2 class="font-bold text-2xl">{{ props.header }}</h2>
-        <div class="flex-1"></div>
-        <button
-          @click="emit('closed-modal')"
-          class="hover:bg-gray-50 rounded-full p-2 -m-2"
-        >
-          <XIcon class="h-8 w-8 text-gray-800" />
-        </button>
-      </div>
-      <div class="h-4"></div>
+    <Transition
+      appear
+      enter-active-class="ease-out duration-300"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+    >
+      <div class="mx-auto max-w-md bg-white rounded-md px-4 py-3">
+        <div class="flex">
+          <h2 class="font-bold text-2xl">{{ props.header }}</h2>
+          <div class="flex-1"></div>
+          <button
+            @click="emit('closed-modal')"
+            class="hover:bg-gray-50 rounded-full p-2 -m-2"
+          >
+            <XIcon class="h-8 w-8 text-gray-800" />
+          </button>
+        </div>
+        <div class="h-4"></div>
 
-      <slot />
-    </div>
+        <slot />
+      </div>
+    </Transition>
   </div>
 </template>
