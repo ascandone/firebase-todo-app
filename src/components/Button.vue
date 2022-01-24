b289304d65e3cd68e912d9e161280fba51c65b18
ascandone <alescandone@gmail.com>
2022-01-24 09:56:40 +0100

    added ghost btn

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-export type Variant = 'primary' | 'danger'
+export type Variant = 'primary' | 'danger' | 'ghost'
 export type Size = 'medium' | 'small'
 
 export interface Props {
@@ -17,12 +17,13 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <button
     :class="{
-      'font-semibold shadow border leading-none rounded-md transition-colors duration-100': true,
-      ' px-3 py-2': props.size === 'medium',
-      ' px-2 py-1.5 text-sm': props.size === 'small',
-      ' bg-gray-50 hover:bg-gray-100': props.variant === 'primary',
-      'bg-gray-50 text-rose-800 hover:bg-rose-800 hover:text-rose-100':
+      'font-semibold leading-none rounded-md transition-colors duration-100': true,
+      'px-3 py-2': props.size === 'medium',
+      'px-2 py-1.5 text-sm': props.size === 'small',
+      'bg-gray-50 hover:bg-gray-100 shadow border': props.variant === 'primary',
+      'bg-gray-50 text-rose-800 hover:bg-rose-800 hover:text-rose-100 shadow border':
         props.variant === 'danger',
+      'text-blue-600 hover:bg-gray-100': props.variant === 'ghost',
     }"
     :type="props.type"
   >
