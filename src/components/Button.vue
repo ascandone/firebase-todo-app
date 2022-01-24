c4d3b8ad515569817d3a3524975bfe87cbf9cd10
ascandone <alescandone@gmail.com>
2022-01-24 09:40:17 +0100

    added medium btn

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
 export type Variant = 'primary' | 'danger'
+export type Size = 'medium' | 'small'
 
 export interface Props {
   variant?: Variant
+  size?: Size
   type?: 'button' | 'submit'
 }
 
 const props = withDefaults(defineProps<Props>(), {
   variant: 'primary',
   type: 'button',
+  size: 'medium',
 })
 </script>
 <template>
   <button
     :class="{
-      'font-semibold shadow border leading-none px-3 py-2 rounded-md transition-colors duration-100': true,
+      'font-semibold shadow border leading-none rounded-md transition-colors duration-100': true,
+      ' px-3 py-2': props.size === 'medium',
+      ' px-2 py-1.5 text-sm': props.size === 'small',
       ' bg-gray-50 hover:bg-gray-100': props.variant === 'primary',
       'bg-gray-50 text-rose-800 hover:bg-rose-800 hover:text-rose-100':
         props.variant === 'danger',
